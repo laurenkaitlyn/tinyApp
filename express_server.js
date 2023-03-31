9bae531a144629b777fb0bc0768b58384cca2dcb
Lauren <laurenkaitlyn@gmail.com>
2023-03-31 01:36:02 -0400

    redirect to url

--- express_server.js
+++ express_server.js
@@ -1,26 +1,61 @@
+//express
 const express = require("express");
 const app = express();
+
+//port
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true }));
+
+//helper
+const generateRandomString = () => {
+  let result = '';
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return result;
+}
+
+//database 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca"},
+  "9sm5xK": {longURL: "http://www.google.com"}
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+
+
+//
+app.post("/urls", (req, res) => {
+  console.log(req.body); //log the POST request body to the console
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = { longURL };
+  res.redirect(`/urls/${shortURL}`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+//adding new url
+app.post("/urls/:id", (req, res) => {
+  let longURL = req.body.longURL;
+  urlDatabase[req.params.id].longURL = longURL;
+  res.redirect('/urls');
+})
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+//main page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -34,6 +69,11 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL:urlDatabase.id};
   res.render("urls_shows", templateVars);
 });
+
+app.get("/u/:id", (req, res) => {
+  let longURL = req.body.longURL;
+  res.redirect(longURL);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
